196af6fe65e0517ff415b87d90631ed7493421e4
caaguirreec <cesarag89@gmail.com>
2025-03-17 18:17:28 -0500

    final tests for contact email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
+          name: form.name,
           to_name: "JavaScript Mastery",
-          from_email: form.email,
+          email: form.email,
           to_email: "sujata@jsmastery.pro",
           message: form.message,
         },
